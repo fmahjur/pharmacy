8c829b1c0f5f659669453e5426a46c77fcd2b991
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-18 14:31:02 +0430

    add Pharmacy class as a entity in model.entity package

--- src/ir/maktab/model/entity/User.java
+++ src/ir/maktab/model/entity/User.java
@@ -1,4 +1,9 @@
 package ir.maktab.model.entity;
 
 public class User {
+    private String firstname;
+    private String lastname;
+    private String nationalCode;
+    private String mobilePhone;
+
 }
