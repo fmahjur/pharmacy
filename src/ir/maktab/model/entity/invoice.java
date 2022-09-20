686a9cb6ec53fb68925680d37977ab11844e6353
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-20 11:10:07 +0430

    add Invoice class body in model.entity package

--- src/ir/maktab/model/entity/invoice.java
+++ src/ir/maktab/model/entity/invoice.java
@@ -1,4 +1,55 @@
 package ir.maktab.model.entity;
 
+import java.util.Objects;
+
 public class invoice {
+    private Patient patient;
+    private Prescription prescription;
+    private double totalBill;
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Prescription getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(Prescription prescription) {
+        this.prescription = prescription;
+    }
+
+    public double getTotalBill() {
+        return totalBill;
+    }
+
+    public void setTotalBill(double totalBill) {
+        this.totalBill = totalBill;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        invoice invoice = (invoice) o;
+        return Double.compare(invoice.totalBill, totalBill) == 0 && Objects.equals(patient, invoice.patient) && Objects.equals(prescription, invoice.prescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(patient, prescription, totalBill);
+    }
+
+    @Override
+    public String toString() {
+        return "invoice{" +
+                "patient=" + patient +
+                ", prescription=" + prescription +
+                ", totalBill=" + totalBill +
+                '}';
+    }
 }
