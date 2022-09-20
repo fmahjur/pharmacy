f361aed9f5ee793bd1b17092117206afc083f354
Reihaneh Mahjour <rihaneh763@gmail.com>
2022-09-20 10:50:47 +0430

    add Admin class body in model.entity package

--- src/ir/maktab/model/entity/Admin.java
+++ src/ir/maktab/model/entity/Admin.java
@@ -3,4 +3,12 @@ package ir.maktab.model.entity;
 public class Admin {
     private final String username = "administrator";
     private final String password = "123456";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
